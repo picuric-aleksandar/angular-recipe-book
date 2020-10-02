78d3281032b41d181eba714d033d3b2ac01bc539
Aleksandar Picuric <apicuric@gmail.com>
2020-10-02 02:57:42 +0200

    Recipe Deatil component v1.1
    Markup almost finished for Recipe Detail component. When we implement service logic then we will update both markup and logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { FormsModule } from '@angular/forms';
 
@@ -19,7 +19,7 @@ import { FormsModule } from '@angular/forms';
     ShoppingEditComponent,
     RecipesComponent,
     RecipeListComponent,
-    RecipeEditComponent,
+    RecipeDetailComponent,
     RecipeItemComponent
   ],
   imports: [
