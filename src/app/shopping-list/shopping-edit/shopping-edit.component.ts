32e196738c75f29a37243f49eb097a205943cc17
Aleksandar Picuric <apicuric@gmail.com>
2020-10-04 12:25:43 +0200

    Shopping edit update 1.2
    Action methods added to the edit component both markup and TS file.
    Next step is to hook up actions from component with one in Shopping service

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('ingredientName',{static: true}) name: ElementRef;
+  @ViewChild('ingredientAmount',{static: true}) amount: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  onAddIngredient(){}
+
+  onUpdateIngredient(){}
+
+  onDeleteIngredient(){}
 }
